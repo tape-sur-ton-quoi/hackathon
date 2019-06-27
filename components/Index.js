c4d90654801cf6072e89c65e99ce0bd112f703e0
matteo farigu <matteo.farigu@gmail.com>
2019-06-27 15:05:05 +0200

    [WIP] enemy animation in progress

--- components/Index.js
+++ components/Index.js
@@ -12,6 +12,7 @@ import {
 
 import Trump from './Trump';
 import Princess from './Princess';
+import Vilain from './Vilain'
 
 export default class Index extends Component {
 
@@ -23,10 +24,60 @@ export default class Index extends Component {
         movePrincessVal: new Animated.Value(40),
         princessSide: 'left',
 
+        moveEnnemiVal: new Animated.Value(0),
+        ennemyStartposX: 0,
+        ennemySide: 'left',
+        ennemySpeed: 4200,
+        
+
         gameOver: false,
     }
+    componentDidMount() {
+        this.animatedEnnemy();
+    }
+    
+    animatedEnnemy() {
+        this.state.moveEnnemiVal.setValue(-100)
+        let windhowH = Dimensions.get('window').height;
 
+        let r = Math.floor(Math.random()*2)+1;
+
+        if (r == 2) {
+            r = 40;
+            this.setState({ennemySide : 'left'})
+        } else {
+            r = Dimensions.get('window').width - 140
+            this.setState({ennemySide : 'right'})
+        }
+        this.setState({ennemyStartposX : r });
+
+        let refreshIntervalId;
+        refreshIntervalId = setInterval(() => {
+            // if (this.state.moveEnnemiVal._value > windowH - 200 && this.state.moveEnnemiVal._value < windhowH - 180 && this.state.ennemySide) {
+            //     clearInterval(refreshIntervalId)
+            //     // this.setState({ })
+            // }
+
+        }, 50)
+
+        setInterval( () => {
+            this.setState({ ennemySpeed: this.state.ennemySpeed - 50})
+        }, 20000)
+
+        Animated.timing(
+          this.state.moveEnnemiVal,
+          {
+              toValue: Dimensions.get('window').height,
+              duration: this.state.ennemySpeed,
+          }
+        ).start(event => {
+            clearInterval(refreshIntervalId);
+            this.setState
+
+        })
+    }
     render() {
+        
         return (
             <View>
                 <ImageBackground source={require('../assets/bg.jpg')}
@@ -36,15 +87,20 @@ export default class Index extends Component {
                         flex: 1,
                         position: 'relative',
                         resizeMode: 'cover',
-
                     }}>
                     <Trump trumpImg={require('../assets/trump.png')}
-                        trumpStartposX={this.state.trumpStartposX} moveTrumpval={this.state.moveTrumpval} />
-
+                    />
                 </ImageBackground>
-                    <Princess princessImg={require('../assets/princess.jpg')} movePrincessVal={this.state.movePrincessVal} />
+
+                <Vilain vilainImg={require('../assets/trump.png')}
+                    ennemyStartposX={this.state.ennemyStartposX}
+                    moveEnnemiVal={this.props.moveEnnemiVal}>
+                </Vilain>
+
+                <Princess princessImg={require('../assets/princess.jpg')} movePrincessVal={this.state.movePrincessVal} />
             </View>
         );
+        
     }
 }
 
---
+++ components/Vilain.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  Animated,
+  Image,
+} from 'react-native';
+
+export default class Vilain extends Component {
+  render (){
+  return (
+    <View>
+      <Animated.Image source={this.props.vilainImg}
+        style={{
+          height: 100,
+          width: 100,
+          position: 'absolute',
+          resizeMode: 'stretch',
+          left: this.props.ennemyStartposX,
+          transform: [
+            { translateY: this.props.moveEnnemyVal }
+          ]
+        }}>
+      </Animated.Image>
+    </View>
+
+  );
+  }
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
