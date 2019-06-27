228ec081b2cc1ca44f898900748cd0adb48c7365
kahija <ouarezki.kahina@gmail.com>
2019-06-28 00:59:48 +0200

    counter not finished

--- components/Score.js
+++ components/Score.js
@@ -9,12 +9,23 @@ import {
 } from "react-native";
 
 export default class Score extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      points: 0,
+    };
+  }
   render() {
     return (
-      <View >
-        <Text
-          style={{fontweight: "bold",fontSize: 40,flex:1,alignItems:"center",marginTop:80}}>
-          </Text>
+      <View>
+        <Text 
+          style={{
+            fontweight: "bold",
+            fontSize: 40,
+            flex: 1,
+            alignItems: "center",
+            marginTop: 80,
+          }}>{styles.points}</Text>
         
       </View>
     );
@@ -23,13 +34,11 @@ export default class Score extends Component {
 
 const styles = StyleSheet.create({
   points: {
-    width:80,
-    height:80,
-    backgroundColor:'#fff',
-    borderRadius:100,
-    alignItems:'center',
-    justifyContent:'center',
-
-    
-  },
-})
+    width: 80,
+    height: 80,
+    backgroundColor: "#fff",
+    borderRadius: 100,
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
