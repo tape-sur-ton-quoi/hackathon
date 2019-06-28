04e6a1612cbab16a4559e85ecd2c6ee66dc5c0f9
Agueda Perez <aguedapm@yahoo.com>
2019-06-28 11:05:14 +0200

    [Code] Added alert logic to Index.js so that the Punchlines alert functions when Princess and Trump collide

--- components/Index.js
+++ components/Index.js
@@ -25,21 +25,17 @@ export default class Index extends Component {
         trumpSide: 'left',
         trumpSpeed: 4200,
 
-        gameOver: false,
+        punchlines: false,
 
+        good: '',
+        bad: ''
     }
 
 
 
     render() {
 
-        var random = Math.floor(Math.random() * Punchlines.length);
-        var bad = Punchlines[random].bad;
-        var good = Punchlines[random].good;
 
-        var gameOver = () => {
-            alert(good)
-        }
 
         return (
             <View>
@@ -52,14 +48,124 @@ export default class Index extends Component {
                         position: 'relative',
                         resizeMode: 'cover',
                     }}>
-                    <Text>{bad}</Text>
+                    
                     <Princess princessImg={require('../assets/princess.jpg')} movePrincessVal={this.state.movePrincessVal} />
                     <Trump trumpImg={require('../assets/trump.png')}
-                        trumpStartposX={this.state.trumpStartposX} moveTrumpVal={this.state.moveTrumpVal} />
+                        trumpStartposX={this.state.trumpStartposX} moveTrumpVal={this.state.moveTrumpVal} bad={this.state.bad} />
+                    <View style={styles.controls}>
+                        <Text style={styles.left} onPress={() => this.movePrincess('left')}> {'<'} </Text>
+                        <Text style={styles.right} onPress={() => this.movePrincess('right')}> {'>'} </Text>
+                    </View>
                 </ImageBackground>
             </View>
 
         );
-     
+
     }
-}
+    componentDidMount = () => {
+        this.animateTrump()
+
+        var random = Math.floor(Math.random() * Punchlines.length);
+        var bad = Punchlines[random].bad;
+        var good = Punchlines[random].good;
+        this.setState({good: good, bad: bad})
+    }
+
+    animateTrump =()=> {
+        this.state.moveTrumpVal.setValue(-100);
+        var windowH = Dimensions.get('window').height;
+
+        var r = Math.floor(Math.random()*2)+1;
+
+        if (r == 2) {
+            r = 40;
+            this.setState({ trumpSide: 'left'});
+        }else {
+            r = Dimensions.get('window').width - 140;
+            this.setState({ trumpSide: 'right'});
+        }
+        this.setState({trumpStartposX: r });
+
+        var refreshIntervalId;
+        refreshIntervalId = setInterval(() => {
+          if(this.state.moveTrumpVal._value > windowH - 280
+            && this.state.moveTrumpVal._value < windowH - 180
+            && this.state.princessSide == this.state.trumpSide) {
+
+                clearInterval(refreshIntervalId)
+                this.setState({ punchlines : true });
+                this.punchlines();
+            }
+        },50);
+
+
+        setInterval(() => {
+        this.setState(({ trumpSpeed: this.state.trumpSpeed - 50}))
+        },20000);
+        Animated.timing(
+           this.state.moveTrumpVal, {
+               toValue: Dimensions.get('window').height,
+               duration: this.state.trumpSpeed,
+           } 
+        ).start(event => {
+            if(event.finished && this.state.punchlines == false) {
+              clearInterval(refreshIntervalId)
+              this.setState({ points: ++this.state.points})
+              this.animateTrump()
+            }
+        })
+    }
+        punchlines() {
+           alert(this.state.good);
+        }
+
+    movePrincess(direction) {
+        //move princess right
+        if (direction == 'right') {
+          this.setState({ princessSide: 'right' });
+          Animated.spring(
+            this.state.movePrincessVal,
+            {
+              toValue: Dimensions.get('window').width - 140,
+              tension: 120,
+            }
+          ).start();
+        } else if (direction == 'left') {
+          this.setState({ princessSide: 'left' });
+          Animated.spring(
+            this.state.movePrincessVal,
+            {
+              toValue: 40,
+              tension: 120,
+            }
+          ).start();
+        }
+      }
+    }
+          const styles = StyleSheet.create({
+            container: {
+              flex: 1,
+              backgroundColor: '#fff',
+              alignItems: 'center',
+              justifyContent: 'center',
+            },
+            controls: {
+              alignItems: 'center',
+              flexDirection: 'row',
+            },
+            right: {
+              flex: 1,
+              color: '#fff',
+              margin: 0,
+              fontSize: 60,
+              fontWeight: 'bold',
+              textAlign: 'left'
+            },
+            left: {
+              flex: 1,
+              color: '#fff',
+              fontSize: 60,
+              fontWeight: 'bold',
+              textAlign: 'right'
+            },
+          });
--- components/Trump.js
+++ components/Trump.js
@@ -9,6 +9,7 @@ import {
     View,
 } from 'react-native';
 
+
 export default class Trump extends Component {
     constructor(props) {
         super(props);
@@ -21,7 +22,7 @@ export default class Trump extends Component {
     }
 
     render() {
-        
+        console.log(this.props.bad)
         return (
             <View>
                 <Animated.Image source={this.props.trumpImg}
@@ -30,65 +31,17 @@ export default class Trump extends Component {
                         width: 100,
                         position: 'absolute',
                         resizeMode: 'stretch',
-                        left: this.state.trumpStartposX,
+                        left: this.props.trumpStartposX,
                         transform: [
-                            { translateY: this.state.moveTrumpVal }
+                            { translateY: this.props.moveTrumpVal }
                         ]
                     }}>
-                </Animated.Image>
                 
+                </Animated.Image>
+                <Text>{this.props.bad}</Text>   
             </View>
             
         )
     }
-    componentDidMount = () => {
-        this.animateTrump()
-    }
-    animateTrump =()=> {
-        this.state.moveTrumpVal.setValue(-100);
-        var windowH = Dimensions.get('window').height;
-
-        var r = Math.floor(Math.random()*2)+1;
-
-        if (r == 2) {
-            r = 40;
-            this.setState({ trumpSide: 'left'});
-        }else {
-            r = Dimensions.get('window').width - 140;
-            this.setState({ trumpSide: 'right'});
-        }
-        this.setState({trumpStartposX: r });
-
-        var refreshIntervalId;
-        refreshIntervalId = setInterval(() => {
-          if(this.state.moveTrumpVal._value > windowH - 280
-            && this.state.moveTrumpVal._value < windowH - 180
-            && this.state.princessSide == this.state.trumpSide) {
-
-                clearInterval(refreshIntervalId)
-                this.setState({ gameOver : true });
-                this.gameOver();
-            }
-        },50);
-
-
-        setInterval(() => {
-        this.setState(({ trumpSpeed: this.state.trumpSpeed - 50}))
-        },20000);
-        Animated.timing(
-           this.state.moveTrumpVal, {
-               toValue: Dimensions.get('window').height,
-               duration: this.state.trumpSpeed,
-           } 
-        ).start(event => {
-            if(event.finished && this.state.gameOver == false) {
-              clearInterval(refreshIntervalId)
-              this.setState({ points: ++this.state.points})
-              this.animateTrump()
-            }
-        })
-    }
-        gameOver() {
-           alert('perdu');
-        }
+  
 }
