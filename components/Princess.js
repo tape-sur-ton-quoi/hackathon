71816a9e5989d1e52fa9d4d81424ab5c3d530094
Rémy Jacob <rjpro77@outlook.fr>
2019-06-28 12:17:26 +0200

    ok

--- components/Princess.js
+++ components/Princess.js
@@ -10,35 +10,30 @@ import {
 } from 'react-native';
 
 export default class Princess extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-   
-    }
-  }
+
   render() {
     return (
       <View>
-
         <Animated.Image source={this.props.princessImg}
           style={{
             height: 130,
             width: 100,
             position: 'absolute',
-            top: 650,
+            top: 550,
             resizeMode: 'stretch',
             transform: [
               { translateX: this.props.movePrincessVal }
             ]
           }}>
         </Animated.Image>
-
-      
-
       </View>
     )
   }
-
 }
+
+
+
+      
+
 
 
